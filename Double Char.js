99ee5e37cecf63786c1e4f751b0f9f11cc929ed0
Alexsus <alexsuslin@inbox.ru>
2022-12-03 18:29:08 +0300

    added 6 kata

---
+++ Double Char.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+  let a = str.split("");
+  return a.map((s) => s.repeat(2)).join("");
+}
